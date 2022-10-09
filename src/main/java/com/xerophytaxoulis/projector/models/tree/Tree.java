3bb27ebc832d32a11bc9628bcdee84e153281426
xerophytaxoulis <suselphoenix@gmx.de>
2022-10-09 22:59:56 +0200

    If a Root is a Leaf, construct a root without children. Also, if an inner node does not have a parent, use a root.

--- src/main/java/com/xerophytaxoulis/projector/models/tree/Tree.java
+++ src/main/java/com/xerophytaxoulis/projector/models/tree/Tree.java
@@ -1,10 +1,15 @@
 package com.xerophytaxoulis.projector.models.tree;
 
+import org.springframework.lang.NonNull;
+
 import java.util.List;
 
 // Recursive definition of a rooted tree
 public sealed interface Tree<T> permits InnerNode, Tree.Leaf {
-    record Leaf<T> (T content, InnerNode<T> parent) implements Tree<T> {}
-    record Root<T> (T content, List<InnerNode<T>> children) implements RootNode<T> {}
-    record Inner<T> (T content, InnerNode<T> parent, List<InnerNode<T>> children) implements InnerNode<T> {}
+    record Leaf<T> (T content, @NonNull InnerNode<T> parent)
+            implements Tree<T> {}
+    record Root<T> (T content, List<InnerNode<T>> children)
+            implements RootNode<T> {}
+    record Inner<T> (T content, @NonNull InnerNode<T> parent, List<InnerNode<T>> children)
+            implements InnerNode<T> {}
 }
