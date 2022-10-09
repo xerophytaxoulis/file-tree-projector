d8560ea5fb184660439177ede2caa1c323cb859a
xerophytaxoulis <suselphoenix@gmx.de>
2022-10-09 18:49:39 +0200

    initial commit

---
+++ src/test/java/com/xerophytaxoulis/projector/ProjectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xerophytaxoulis.projector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class ProjectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
