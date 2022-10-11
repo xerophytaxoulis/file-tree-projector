f51fab87fd77eb52dec7a82dcedaf067243015b9
xerophytaxoulis <suselphoenix@gmx.de>
2022-10-11 23:10:37 +0200

    for the sake of a better name

--- src/main/java/com/xerophytaxoulis/projector/models/tree/RootedTree.java
+++ src/main/java/com/xerophytaxoulis/projector/models/tree/RootedTree.java
@@ -45,36 +45,36 @@ public sealed interface RootedTree<T, L> permits InnerNode, TerminalNode {
 
     // TODO: Test them, why is Intellij saying that only the first branch is reachable?
     default Inner<T, L> toInner(InnerNode<T, L> newParent, List<RootedTree<T, L>> newChildren,
-                                       Function<L, L> labelTransform) {
+                                       Function<L, L> labelTransformation) {
         return switch (this) {
             case Leaf<T, L> (T container,L label,InnerNode<T, L> parent) ->
-                    new Inner<> (container, labelTransform.apply(label), parent, newChildren);
+                    new Inner<> (container, labelTransformation.apply(label), parent, newChildren);
             case Inner<T, L> (T container,L label, InnerNode<T, L> parent, List<RootedTree<T, L>> children) ->
-                    new Inner<> (container, labelTransform.apply(label), parent, children);
+                    new Inner<> (container, labelTransformation.apply(label), parent, children);
             case Root<T, L> (T container,L label,List<RootedTree<T, L>> children) ->
-                    new Inner<> (container, labelTransform.apply(label), newParent, children);
+                    new Inner<> (container, labelTransformation.apply(label), newParent, children);
         };
     }
 
-    default Leaf<T, L> toLeaf (InnerNode<T, L> newParent, Function<L, L> labelTransform) {
+    default Leaf<T, L> toLeaf (InnerNode<T, L> newParent, Function<L, L> labelTransformation) {
         return switch (this) {
             case Leaf<T, L> (T container,L label,InnerNode<T, L> parent) ->
-                    new Leaf<> (container, labelTransform.apply(label), parent);
+                    new Leaf<> (container, labelTransformation.apply(label), parent);
             case Inner<T, L> (T container,L label, InnerNode<T, L> parent, List<RootedTree<T, L>> ignored) ->
-                    new Leaf<> (container, labelTransform.apply(label), parent);
+                    new Leaf<> (container, labelTransformation.apply(label), parent);
             case Root<T, L> (T container,L label,List<RootedTree<T, L>> ignored) ->
-                    new Leaf<> (container, labelTransform.apply(label), newParent);
+                    new Leaf<> (container, labelTransformation.apply(label), newParent);
         };
     }
 
-    default Root<T, L> toRoot(List<RootedTree<T, L>> newChildren, Function<L, L> labelTransform) {
+    default Root<T, L> toRoot(List<RootedTree<T, L>> newChildren, Function<L, L> labelTransformation) {
         return switch(this) {
             case Leaf<T, L> (T container,L label,InnerNode<T, L> parent) ->
-                    new Root<> (container, labelTransform.apply(label), newChildren);
+                    new Root<> (container, labelTransformation.apply(label), newChildren);
             case Inner<T, L> (T container,L label, InnerNode<T, L> ignored, List<RootedTree<T, L>> children) ->
-                    new Root<> (container, labelTransform.apply(label), children);
+                    new Root<> (container, labelTransformation.apply(label), children);
             case Root<T, L> (T container,L label,List<RootedTree<T, L>> ignored) ->
-                    new Root<> (container, labelTransform.apply(label), newChildren);
+                    new Root<> (container, labelTransformation.apply(label), newChildren);
         };
     }
 
